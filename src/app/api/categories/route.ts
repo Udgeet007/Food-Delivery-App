500d4edfe67b816a1a12df979eefd4db990eb034
Udgeet <bhatt8udgeet@gmail.com>
2024-08-18 02:00:07 +0530

    Created API CRUD with Prisma.

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,9 +1,23 @@
-import { NextResponse } from "next/server"
 
-export const GET = () => {
-  return new NextResponse("Hello", {status:200})
-}
+import { prisma } from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+
+// Fetch all Categories
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
 
 export const POST = () => {
-  return new NextResponse("Hello", {status:200})
-}
+  return new NextResponse("Hello", { status: 200 });
+};
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,8 +1,21 @@
-import { featuredProducts } from "@/data";
+// import { featuredProducts } from "@/data";
+import { ProductType } from "@/types/types";
 import Image from "next/image";
 import React from "react";
 
-const Featured = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/products", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed!");
+  }
+  return res.json();
+};
+
+const Featured = async () => {
+  const featuredProducts:ProductType[] = await getData()
   return (
     <div className="w-screen overflow-x-scroll text-red-500">
       {/* WRAPPER */}
@@ -21,7 +34,9 @@ const Featured = () => {
             )}
             {/* TEXT CONTAINER */}
             <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
-              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                {item.title}
+              </h1>
               <p className="p-4 2xl:p-8">{item.desc}</p>
               <span className="text-xl font-bold">${item.price}</span>
               <button className="bg-red-500 text-white p-2 rounded-md">

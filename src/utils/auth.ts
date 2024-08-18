4cf2bbd901488fde9b521c88dfa0a096a7bdedf2
Udgeet <bhatt8udgeet@gmail.com>
2024-08-18 23:03:11 +0530

    created Models

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,10 @@
-import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { getServerSession, NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./connect";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
@@ -9,3 +12,5 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 };
+
+export const getAuthSession = () => getServerSession(authOptions)

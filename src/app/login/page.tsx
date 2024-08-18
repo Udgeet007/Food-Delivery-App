0ff4fe656c3b04c0ec8d0c748224e921a9fb21ab
Udgeet <bhatt8udgeet@gmail.com>
2024-08-18 16:46:30 +0530

    Used Next Auth for Authentication.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,29 @@
+"use client";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const LoginPage = () => {
+  const { data, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p> Loading...</p>;
+  }
+
+  if (status === "authenticated"){
+    router.push("/")
+  }
+
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/* BOX */}
       <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
         {/* IMAGE CONTAINER */}
         <div className="relative h-1/3 w-full md:h-full md:w-1/2">
-          <Image src="/loginBg.png" alt="" fill className="object-cover"/>
+          <Image src="/loginBg.png" alt="" fill className="object-cover" />
         </div>
         {/* FORM CONTAINER */}
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
@@ -25,7 +39,10 @@ const LoginPage = () => {
             />
             <span>Sign in with Google</span>
           </button>
-          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+          <button
+            className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md"
+            onClick={() => signIn("google")}
+          >
             <Image
               src="/facebook.png"
               alt=""
@@ -36,7 +53,11 @@ const LoginPage = () => {
             <span>Sign in with Facebook</span>
           </button>
           <p className="text-sm">
-            Have a problem?<Link className="underline" href="/"> Contact us</Link>
+            Have a problem?
+            <Link className="underline" href="/">
+              {" "}
+              Contact us
+            </Link>
           </p>
         </div>
       </div>

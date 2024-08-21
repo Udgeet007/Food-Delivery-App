e29364c957c0be048f575e0ae5307ff9e11a620b
Udgeet <bhatt8udgeet@gmail.com>
2024-08-21 23:55:00 +0530

    changes

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,27 +1,60 @@
+import { getAuthSession } from "@/utils/auth";
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
-// FETCH ALL Orders
+// FETCH ALL ORDERS
 export const GET = async (req: NextRequest) => {
-  const { searchParams } = new URL(req.url);
-  const cat = searchParams.get("cat");
+  const session = await getAuthSession();
 
-  try {
-    const products = await prisma.product.findMany({
-      where: {
-        ...(cat ? { catSlug: cat } : { isFeatured: true }),
-      },
-    });
-    return new NextResponse(JSON.stringify(products), { status: 200 });
-  } catch (err) {
-    console.log(err);
+  if (session) {
+    try {
+      if (session.user.isAdmin) {
+        const orders = await prisma.order.findMany();
+        return new NextResponse(JSON.stringify(orders), { status: 200 });
+      }
+      const orders = await prisma.order.findMany({
+        where: {
+          userEmail: session.user.email!,
+        },
+      });
+      return new NextResponse(JSON.stringify(orders), { status: 200 });
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(
+        JSON.stringify({ message: "Something went wrong!" }),
+        { status: 500 }
+      );
+    }
+  } else {
     return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }),
-      { status: 500 }
+      JSON.stringify({ message: "You are not authenticated!" }),
+      { status: 401 }
     );
   }
 };
 
-export const POST = () => {
-  return new NextResponse("Hello", { status: 200 });
+// CREATE ORDER
+export const POST = async (req: NextRequest) => {
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const body = await req.json();
+      const order = await prisma.order.create({
+        data: body,
+      });
+      return new NextResponse(JSON.stringify(order), { status: 201 });
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(
+        JSON.stringify({ message: "Something went wrong!" }),
+        { status: 500 }
+      );
+    }
+  } else {
+    return new NextResponse(
+      JSON.stringify({ message: "You are not authenticated!" }),
+      { status: 401 }
+    );
+  }
 };
